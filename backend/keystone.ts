23fac4cf6ec2a98948661d7f8884255a5cd2433a
ElMayo <mic.l.meier@gmail.com>
2021-08-05 17:54:22 +0200

    bugfix: Fix relative paths in tsconfig

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,9 +1,8 @@
+import { lists } from '@backend/schema'
+import { rules } from '@backend/schema/access'
 import { createAuth } from '@keystone-next/auth'
 import { config } from '@keystone-next/keystone/schema'
 import { statelessSessions } from '@keystone-next/keystone/session'
-
-import { lists } from './schema'
-import { rules } from './schema/access'
 
 const dbUrl =
   process.env.DATABASE_URL ||
